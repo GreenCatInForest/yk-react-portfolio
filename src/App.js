67f1fe1f871af94fa1a559139089e192b25324bc
GreenCatInForest <event2016.2017@gmail.com>
2023-10-21 21:54:46 +0100

    import containers to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
+import { Home } from "./containers/Home";
+import { Portfolio } from "./containers/Portfolio";
+import { AboutMe } from "./containers/AboutMe";
+import { ContactMe } from "./containers/ContactMe";
+
+import { Footer } from "./components/Footer";
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Home />
+      <Portfolio />
+      <AboutMe />
+      <ContactMe />
+      <Footer />
+    </div>
+  );
 };
---
+++ src/containers/Portfolio.js
@@ -0,0 +1,3 @@
+export const Portfolio = () => {
+  return <div>My Projects</div>;
+};
