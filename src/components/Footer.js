24518daf47e88c58be61922154681dc3feb12142
GreenCatInForest <event2016.2017@gmail.com>
2023-10-22 21:44:12 +0100

    add footer html

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,12 @@
+import { SocialMedia } from "./SocialMedia";
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <h2>Lorem ipsum</h2>
+      <h3>Dolor sit amet</h3>
+      <SocialMedia />
+      <p>YK, 2023</p>
+    </div>
+  );
 };
--- src/containers/ContactMe.js
+++ src/containers/ContactMe.js
@@ -3,8 +3,15 @@ import { ContactForm } from "../components/ContactForm";
 export const ContactMe = () => {
   return (
     <div id="contact">
-      <h2>Contact Me</h2>
+      <h2>Contact</h2>
+      <h3>Let's work together!</h3>
+      <button>Get In Touch</button>
       <ContactForm />
+      <h3>Contact me directly:</h3>
+      <img alt="icon phone"></img>
+      <p>phone number</p>
+      <img alt="icon mail"></img>
+      <p>mail address</p>
     </div>
   );
 };
