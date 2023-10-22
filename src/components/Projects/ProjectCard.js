2792ee7b646b72b3b1f4fd2f8a382aa52151abbc
GreenCatInForest <event2016.2017@gmail.com>
2023-10-22 20:18:16 +0100

    add project card html

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,3 +1,11 @@
 export const ProjectCard = () => {
-  return <div>ProjectCard</div>;
+  return (
+    <div>
+      <img src="#" alt="project banner" />
+      <h2>Title</h2>
+      <h3>Subtitle</h3>
+      <button>Wiev</button>
+      <button>Github</button>
+    </div>
+  );
 };
