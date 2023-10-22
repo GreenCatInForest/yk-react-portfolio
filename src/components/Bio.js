7b850ff918cd40e5966a134cf7518aa38a6daecb
GreenCatInForest <event2016.2017@gmail.com>
2023-10-22 20:32:00 +0100

    add bio component html

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,3 +1,23 @@
 export const Bio = () => {
-  return <div>Bio</div>;
+  return (
+    <div>
+      <img src="#" alt="Avatar"></img>
+      <p>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint
+          voluptatum, iusto dicta cum velit amet consequatur temporibus hic nisi
+          quas est non atque iure magni voluptates inventore ea repudiandae
+          adipisci!
+        </span>
+      </p>
+      <p>
+        <span>
+          Repellendus reprehenderit dolor vel accusantium dicta magni blanditiis
+          alias facere dolores doloremque, expedita labore nostrum? Deserunt
+          dignissimos reiciendis ducimus illum totam, vero perspiciatis in!
+          Placeat laboriosam aliquid beatae mollitia expedita!
+        </span>
+      </p>
+    </div>
+  );
 };
