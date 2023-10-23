336f92c1abee3c72fd2107c376047745218101a2
GreenCatInForest <event2016.2017@gmail.com>
2023-10-23 18:28:39 +0100

    add social media icons

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,10 +1,24 @@
+import fbImg from "../assets/images/social/icons8-facebook-30.png";
+import gitImg from "../assets/images/social/icons8-github-30.png";
+import lnkdlnImg from "../assets/images/social/icons8-linkedin-30.png";
+import wtspImg from "../assets/images/social/icons8-whatsapp-30.png";
+
 export const SocialMedia = () => {
   return (
-    <div>
-      <a href="#">FB</a>
-      <a href="#">GitHub</a>
-      <a href="#">Linkedn</a>
-      <a href="#">Pinterest</a>
+    <div className="flex flex-row gap-2">
+      <a href="#">
+        <img src={fbImg} alt="logo facebook" />
+      </a>
+      <a href="#">
+        <img src={gitImg} alt="logo github" />
+      </a>
+      <a href="#">
+        {" "}
+        <img src={lnkdlnImg} alt="logo linkedin" />
+      </a>
+      <a href="#">
+        <img src={wtspImg} alt="logo whatsapp" />
+      </a>
     </div>
   );
 };
