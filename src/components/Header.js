3b47adbd021bfa6dd4992c3afa8e253800addc75
GreenCatInForest <event2016.2017@gmail.com>
2023-10-23 17:07:49 +0100

    style hero.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,16 @@ export const Header = () => {
   return (
     <div
       id="header"
-      className="flex flex-col sm:flex-row justify-around align-middle items-center m-6"
+      className="flex flex-col align-middle items-center sm:flex-row justify-around m-6"
 
-      // justify-center align-middle
+      //
     >
       <h1 id="headerTitle" className="text-3xl font-bold">
         YK
       </h1>
 
       <nav id="headerNav">
-        <ul className="flex flex-col sm:flex-row space-x-8 cursor-pointer">
+        <ul className="flex flex-col sm:flex-row space-x-0 sm:space-x-8 cursor-pointer text-center m-6">
           <li>
             <a href="#home">Home</a>
           </li>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,18 +3,30 @@ import { SocialMedia } from "./SocialMedia";
 
 export const Hero = () => {
   return (
-    <div id="hero">
-      <h1>
-        Hello, I'm <span>YANA</span>
-      </h1>
-      <h2>JUNIOR FRONTEND DEVELOPER</h2>
-      <img
-        // src={bioImg}
-        alt="my large avatar"
-      />
-      <button>Button</button>
-      <button>Button</button>
-      <SocialMedia />
+    <div
+      id="hero"
+      className="flex flex-col align-middle items-center text-center sm:flex-row justify-around"
+    >
+      <article className="flex flex-col align-middle items-center text-center gap-2">
+        <h1 className="text-2xl font-bold">
+          Hello, I'm <span className="text-3xl font-bold">YANA</span>
+        </h1>
+        <h2>Junior Frontend Developer</h2>
+        <button className="bg-orange-600 hover:bg-orange-400 text-white border border-orange-700 w-40 h-10 rounded-full m-1">
+          Projects
+        </button>
+        <button className="bg-orange-400 hover:bg-orange-600 text-white border border-orange-700 w-40 h-10 rounded-full m-1">
+          CV
+        </button>
+        <SocialMedia />
+      </article>
+
+      <article>
+        <img
+          // src={bioImg}
+          alt="my large avatar"
+        />
+      </article>
     </div>
   );
 };
