a7f5fcfefde08a586444400606be3e9b93bbd6ff
GreenCatInForest <event2016.2017@gmail.com>
2023-10-23 21:30:43 +0100

    style hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,15 @@
-// import bioImg from "../assets/images/logo192.png";
+import bioImg from "../assets/images/Untitled3.png";
 import { SocialMedia } from "./SocialMedia";
 
 export const Hero = () => {
   return (
     <div
       id="hero"
-      className="flex flex-col align-middle items-center text-center sm:flex-row justify-around"
+      className="w-11/12 flex flex-col align-middle items-center sm:items-center text-center sm:flex-row"
     >
-      <article className="flex flex-col align-middle items-center text-center gap-2">
-        <h1 className="text-2xl font-bold">
-          Hello, I'm <span className="text-3xl font-bold">YANA</span>
+      <article className="w-3/5 flex flex-col align-middle items-center text-center gap-3 mt-40">
+        <h1 className="text-3xl font-bold">
+          Hello, I'm <span className="text-4xl font-bold">YANA</span>
         </h1>
         <h2>Junior Frontend Developer</h2>
         <button className="bg-orange-600 hover:bg-orange-400 text-white border border-orange-700 w-40 h-10 rounded-full m-1">
@@ -20,13 +20,7 @@ export const Hero = () => {
         </button>
         <SocialMedia />
       </article>
-
-      <article>
-        <img
-          // src={bioImg}
-          alt="my large avatar"
-        />
-      </article>
+      <img src={bioImg} alt="my large avatar" className="w-2/5" />
     </div>
   );
 };
