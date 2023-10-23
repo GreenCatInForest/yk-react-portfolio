95bc71d11241d08bcf781d553de5fcd694567cad
GreenCatInForest <event2016.2017@gmail.com>
2023-10-23 14:29:49 +0100

    add tailwind

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { Banner } from "./components/Banner";
 export const App = () => {
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Home />
       <Projects />
       <Banner />
