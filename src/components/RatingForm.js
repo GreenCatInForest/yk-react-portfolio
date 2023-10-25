a1b91dd9bc0eb10e54a6d3715a3174865b3223a1
GreenCatInForest <event2016.2017@gmail.com>
2023-10-25 21:10:51 +0100

    style contact directly and raiting

--- src/components/RatingForm.js
+++ src/components/RatingForm.js
@@ -1,6 +1,7 @@
 export const RatingForm = () => {
   return (
     <div>
+      <h3>Rate my portfolio:</h3>
       <form>
         <label htmlFor="star5">☆</label>
         <input id="star5" name="star" type="radio" value="5" />
@@ -14,7 +15,9 @@ export const RatingForm = () => {
         <input id="star1" name="star" type="radio" value="1" />
         <div className="clear"></div>
         <label htmlFor="feedbackRating"></label>
-        <button type="submit">Rate Now</button>
+        <button type="submit" className="btn-orange">
+          Rate Now
+        </button>
       </form>
     </div>
   );
--- src/containers/ContactMe.js
+++ src/containers/ContactMe.js
@@ -3,22 +3,25 @@ import { RatingForm } from "../components/RatingForm";
 
 export const ContactMe = () => {
   return (
-    <div
-      id="contact"
-      className="containerContactMe"
-      // className=""
-    >
+    <div id="contact" className="containerContactMe">
       <h2 className="containerTitleH2">Contact Me!</h2>
       <h3 className="text-4xl font-bold">Let's work together!</h3>
       <button className="btn-orange">Get In Touch</button>
       <ContactForm />
-      <h3>Contact me directly:</h3>
-      <img alt="icon phone"></img>
-      <p>phone number</p>
-      <img alt="icon mail"></img>
-      <p>mail address</p>
-      <h3>Rate my portfolio:</h3>
-      <RatingForm />
+
+      <div className="contactMeFeatures">
+        <div className="contactDirectly">
+          <h3>Contact me directly:</h3>
+          <img alt="icon phone"></img>
+          <p>phone number</p>
+          <img alt="icon mail"></img>
+          <p>mail address</p>
+        </div>
+
+        <div className="ratePortfolio">
+          <RatingForm />
+        </div>
+      </div>
     </div>
   );
 };
