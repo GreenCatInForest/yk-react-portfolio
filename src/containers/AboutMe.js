fe86abe38e3af7a515481242e2b5453740020de4
GreenCatInForest <event2016.2017@gmail.com>
2023-10-25 20:57:51 +0100

    center about me contact me

--- src/containers/AboutMe.js
+++ src/containers/AboutMe.js
@@ -3,7 +3,7 @@ import { TechStack } from "../components/TechStack";
 
 export const AboutMe = () => {
   return (
-    <div id="about">
+    <div id="about" className="containerAboutMe">
       <h2 className="containerTitleH2">About Me</h2>
       <Bio />
       <TechStack />
--- src/containers/ContactMe.js
+++ src/containers/ContactMe.js
@@ -5,7 +5,8 @@ export const ContactMe = () => {
   return (
     <div
       id="contact"
-      // className="flex flex-col text-center items-center gap-10 sm:gap-12"
+      className="containerContactMe"
+      // className=""
     >
       <h2 className="containerTitleH2">Contact Me!</h2>
       <h3 className="text-4xl font-bold">Let's work together!</h3>
