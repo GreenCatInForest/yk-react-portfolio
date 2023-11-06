1dcb7600027c3f0946b261a24dd338c5e8a6b274
GreenCatInForest <event2016.2017@gmail.com>
2023-11-06 23:41:10 +0000

    pass project datas as a props

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,17 +1,18 @@
 import gitImg from "../../assets/images/social/icons8-github-30.png";
 
-export const ProjectCard = () => {
+export const ProjectCard = (props) => {
   return (
     <div className="projectCard">
-      <img src="#" alt="project banner" />
-      <h2>Title</h2>
-      <h3>Subtitle</h3>
+      {/* <img src={props.projectBannerImg} alt={props.projectTitle} /> */}
+      <h2>{props.projectTitle}</h2>
+      <h3>{props.projectSubTitle}</h3>
+      {/* <h4>{props.projectDescription}</h4> */}
       <div className="btn-projectCardContainer">
         <button className="btn-orange">
-          <a href="#">Wiev</a>
+          <a href={props.projectDeployUrl}>Wiev</a>
         </button>
         <button className="">
-          <a href="#">
+          <a href={props.projectGitUrl}>
             <img src={gitImg} alt="logo github" />
           </a>
         </button>
--- src/components/Projects/ProjectCardContainer.js
+++ src/components/Projects/ProjectCardContainer.js
@@ -1,16 +1,57 @@
 import { ProjectCard } from "./ProjectCard";
 
+let projectDatas = [
+  {
+    projectBannerImg:
+      "../assets/images/lucas-george-wendt-8ZGgg6rhzxs-unsplash.jpg",
+    projectTitle: "Counter App",
+    projectSubTitle: "Increment, Reset, Decrement the value",
+    projectDescription:
+      "App created to show an example of using Function and Class Components and a useState hook in React",
+    projectDeployUrl: "https://counter-app-react-sigma.vercel.app/",
+    projectGitUrl: "https://github.com/GreenCatInForest/counter-app",
+  },
+  {
+    projectBannerImg: "",
+    projectTitle: "Title",
+    projectSubTitle: "SubTitle",
+    projectDescription:
+      "App created to show an example of using Function and Class Components and a useState hook in React",
+    projectDeployUrl: "https://counter-app-react-sigma.vercel.app/",
+    projectGitUrl: "https://github.com/GreenCatInForest/counter-app",
+  },
+  {
+    projectBannerImg: "",
+    projectTitle: "Title",
+    projectSubTitle: "SubTitle",
+    projectDescription:
+      "App created to show an example of using Function and Class Components and a useState hook in React",
+    projectDeployUrl: "https://counter-app-react-sigma.vercel.app/",
+    projectGitUrl: "https://github.com/GreenCatInForest/counter-app",
+  },
+  {},
+];
+
+console.log(projectDatas);
+
+// projectDatas.map((projectData) => ({}))
+
 export const ProjectCardContainer = () => {
   return (
     <div className="projectCardContainer">
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
+      {projectDatas.map((projectData) => {
+        return (
+          <ProjectCard
+            // projectBannerImg={projectData.projectBannerImg}
+            projectTitle={projectData.projectTitle}
+            projectSubTitle={projectData.projectSubTitle}
+            projectDescription={projectData.projectDescription}
+            projectDeployUrl={projectData.projectDeployUrl}
+            projectGitUrl={projectData.projectGitUrl}
+            key={projectData.projectTitle}
+          />
+        );
+      })}
     </div>
   );
 };
