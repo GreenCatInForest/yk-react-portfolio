3d952193ef814cd7e415b585423a7bcd314ed871
GreenCatInForest <event2016.2017@gmail.com>
2023-11-07 14:41:18 +0000

    add social media links

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -6,17 +6,16 @@ import wtspImg from "../assets/images/social/icons8-whatsapp-30.png";
 export const SocialMedia = () => {
   return (
     <div className="socialMedia">
-      <a href="#">
+      {/* <a href="https://www.facebook.com/profile.php?id=100072930447106">
         <img src={fbImg} alt="logo facebook" />
-      </a>
-      <a href="#">
+      </a> */}
+      <a href="https://github.com/GreenCatInForest">
         <img src={gitImg} alt="logo github" />
       </a>
-      <a href="#">
-        {" "}
+      <a href="https://www.linkedin.com/in/yana-kozarenko-237ba5191/">
         <img src={lnkdlnImg} alt="logo linkedin" />
       </a>
-      <a href="#">
+      <a href="https://wa.me/447285643593">
         <img src={wtspImg} alt="logo whatsapp" />
       </a>
     </div>
