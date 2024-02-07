dc53fef2183a2da33bbcd10e59708afc9ab76e3a
GreenCatInForest <event2016.2017@gmail.com>
2024-02-07 18:27:59 +0000

    style toggle burger button

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,41 +17,10 @@ export const Header = () => {
         YK
       </h1>
 
-      <NavHumburgerMenu hamburgerMenuIsOpen={hamburgerMenuIsOpen} />
-
-      <button
-        onClick={toggleHamburgerMenu}
-        id="toggleBtnMenu"
-        className="text-3xl cursor-pointer relative w-8 h-8"
-      >
-        <div
-          id="hamburgerMenu"
-          className="bg-black w-8 h-1 rounded
-          absolute top-4 -mt-.5
-          transition-all duration-700
-          before:content-''
-          before:bg-black
-          before:w-8
-          before:h-1
-          before:rounded
-          before:absolute
-          before:-translate-x-4
-          before:-translate-y-3
-          before:transition-all
-          before:duration-700
-          after: content-''
-          after:bg-black
-          after:w-8
-          after:h-1
-          after:rounded
-          after:absolute
-          after:-translate-x-4
-          after:translate-y-3
-          after:transition-all
-          after:duration-700
-"
-        ></div>
-      </button>
+      <NavHumburgerMenu
+        toggleHamburgerMenu={toggleHamburgerMenu}
+        hamburgerMenuIsOpen={hamburgerMenuIsOpen}
+      />
       <button className="btn-orange">
         <a href="#downloadCv">Download CV</a>
       </button>
--- src/components/NavHumbergerMenu.js
+++ src/components/NavHumbergerMenu.js
@@ -1,23 +1,41 @@
-export const NavHumburgerMenu = ({ hamburgerMenuIsOpen }) => {
+export const NavHumburgerMenu = ({
+  toggleHamburgerMenu,
+  hamburgerMenuIsOpen,
+}) => {
   if (!hamburgerMenuIsOpen) {
-    return "";
+    return (
+      <button
+        onClick={toggleHamburgerMenu}
+        className="text-3xl cursor-pointer relative w-8 h-8"
+      >
+        <div className="hamburgerMenuOpen"></div>
+      </button>
+    );
   } else
     return (
-      <nav id="headerNav">
-        <ul className="headerNavContainer">
-          <li className="headerNavLink">
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
+      <div>
+        <nav id="headerNav">
+          <ul className="headerNavContainer">
+            <li className="headerNavLink">
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <button
+          onClick={toggleHamburgerMenu}
+          className="text-3xl cursor-pointer relative w-8 h-8"
+        >
+          <div className="hamburgerMenuClose"></div>
+        </button>
+      </div>
     );
 };
