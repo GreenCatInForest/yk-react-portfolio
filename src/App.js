f9066b840cdb50b92656ce004d8164012bf9da5e
GreenCatInForest <event2016.2017@gmail.com>
2024-02-07 14:10:22 +0000

    delete useless block

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Home } from "./containers/Home";
 import { Projects } from "./containers/Projects";
-import { AboutMe } from "./containers/AboutMe";
+import { TechStack } from "./components/TechStack";
 import { ContactMe } from "./containers/ContactMe";
 
 import { Footer } from "./components/Footer";
@@ -13,8 +13,7 @@ export const App = () => {
       <Home />
       <Projects />
       <Banner />
-      <AboutMe />
-      <Banner />
+      <TechStack />
       <ContactMe />
       <Footer />
     </div>
