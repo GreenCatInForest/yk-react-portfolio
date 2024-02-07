9c873ffb398373d7fb592ef1bbe8fdddabf8a3fc
GreenCatInForest <event2016.2017@gmail.com>
2024-02-07 18:54:12 +0000

    styled burger menu

--- src/components/NavHumbergerMenu.js
+++ src/components/NavHumbergerMenu.js
@@ -13,29 +13,29 @@ export const NavHumburgerMenu = ({
     );
   } else
     return (
-      <div>
-        <nav id="headerNav">
-          <ul className="headerNavContainer">
-            <li className="headerNavLink">
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-        </nav>
-        <button
-          onClick={toggleHamburgerMenu}
-          className="text-3xl cursor-pointer relative w-8 h-8"
-        >
-          <div className="hamburgerMenuClose"></div>
-        </button>
-      </div>
+      <nav id="headerNav">
+        <ul className="headerNavContainer">
+          <li className="headerNavLink">
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <button
+              onClick={toggleHamburgerMenu}
+              className="text-3xl cursor-pointer relative w-8 h-8"
+            >
+              <div className="hamburgerMenuClose"></div>
+            </button>
+          </li>
+        </ul>
+      </nav>
     );
 };
