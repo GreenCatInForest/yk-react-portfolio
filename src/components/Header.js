33abea660f9bb57acc1f42f2e050977b0f44e994
GreenCatInForest <event2016.2017@gmail.com>
2024-02-07 21:12:04 +0000

    add light mode icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { NavHumburgerMenu } from "./NavHumbergerMenu";
 import DarkModeImage from "../assets/images/icons8-dark-mode-50.png";
+import LightModeImage from "../assets/images/icons8-light-mode-50.png";
 
 export const Header = ({ setDarkMode, darkMode }) => {
   const [hamburgerMenuIsOpen, setHamburgerMenuIsOpen] = useState(false);
@@ -18,16 +19,24 @@ export const Header = ({ setDarkMode, darkMode }) => {
   };
 
   return (
-    <div id="header" className="headerNav">
+    <div
+      id="header"
+      className={`headerNav ${darkMode ? "darkMode" : "lightMode"}`}
+    >
       <h1 id="headerTitle" className="text-3xl font-bold">
         YK
       </h1>
       <NavHumburgerMenu
+        className={`${darkMode ? "darkMode" : "lightMode"}`}
         toggleHamburgerMenu={toggleHamburgerMenu}
         hamburgerMenuIsOpen={hamburgerMenuIsOpen}
       />
       <button className="darkModeButton" onClick={toggleDarkMode}>
-        <img src={DarkModeImage} alt="Dark Mode Icon" />
+        {!darkMode ? (
+          <img src={DarkModeImage} alt="Dark Mode Icon" />
+        ) : (
+          <img src={LightModeImage} alt="Light Mode Icon" />
+        )}
       </button>
       <button className="btn-orange">
         <a href="#downloadCv">Download CV</a>
