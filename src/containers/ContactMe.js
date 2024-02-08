5e3725e5ed0dcec04eb13bd0df53641329d7d4e2
GreenCatInForest <event2016.2017@gmail.com>
2024-02-08 21:28:01 +0000

    style contact component

--- src/containers/ContactMe.js
+++ src/containers/ContactMe.js
@@ -4,6 +4,9 @@ import { RatingForm } from "../components/RatingForm";
 import phoneIconLight from "../assets/images/icons8-phone-light.png";
 import phoneIconDark from "../assets/images/icons8-phone-dark.png";
 
+import mailIconLight from "../assets/images/icons8-email-light.png";
+import mailIconDark from "../assets/images/icons8-email-dark.png";
+
 export const ContactMe = ({ darkMode }) => {
   return (
     <div id="contact" className="containerContactMe">
@@ -14,24 +17,29 @@ export const ContactMe = ({ darkMode }) => {
       <div className="contactMeFeatures">
         <div className="contactDirectly">
           <h3 className="componentTitle">Contact me directly:</h3>
-          <ul className="contactPhone">
-            {!darkMode ? (
+          {!darkMode ? (
+            <ul>
               <li>
                 <img src={phoneIconLight} alt="Dark Mode Phone Icon" />
                 <p>07827643593</p>
               </li>
-            ) : (
+              <li>
+                <img src={mailIconLight} alt="Dark Mode Mail Icon" />
+                <p>mail address</p>
+              </li>
+            </ul>
+          ) : (
+            <ul>
               <li>
                 <img src={phoneIconDark} alt="Light Mode Phone Icon" />
                 <p>07827643593</p>
               </li>
-            )}
-          </ul>
-
-          <div className="contactMail">
-            <img alt="icon mail"></img>
-            <p>mail address</p>
-          </div>
+              <li>
+                <img src={mailIconDark} alt="Light Mode Mail Icon" />
+                <p>mail address</p>
+              </li>
+            </ul>
+          )}
         </div>
 
         <div className="ratePortfolio">
