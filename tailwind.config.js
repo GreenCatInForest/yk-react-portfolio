087101b085b389604d2bc138b4e90d79f75795bb
GreenCatInForest <event2016.2017@gmail.com>
2024-02-10 19:27:04 +0000

    add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,11 +39,11 @@ module.exports = {
 
         customBounce: {
           "0%, 100%": {
-            transform: "translateY(-25%)",
+            transform: "translateY(25%)",
             animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)",
           },
           "50%": {
-            transform: "translateY(0)",
+            transform: "translateY(0%)",
             animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
           },
         },
