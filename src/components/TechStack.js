a085260b986ed4882601e4c99ef6a9127ba2859e
GreenCatInForest <event2016.2017@gmail.com>
2024-02-10 02:22:01 +0000

    add tech stack items

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -4,6 +4,8 @@ import CssLogo from "../assets/images/tech/CSS3_logo.svg";
 import TailwindLogo from "../assets/images/tech/tailwind-logo.png";
 import BootstrapLogo from "../assets/images/tech/bootstrap-logo.svg";
 import MuiLogo from "../assets/images/tech/mui-logo.png";
+import FigmaLogo from "../assets/images/tech/Figma-Icon.svg";
+import PsLogo from "../assets/images/tech/ps_appicon.svg";
 
 export const TechStack = () => {
   return (
@@ -16,6 +18,8 @@ export const TechStack = () => {
         <img src={TailwindLogo} alt="Tailwind logo" id="TailwindLogo"></img>
         <img src={BootstrapLogo} alt="Bootstrap logo" id="BootstrapLogo"></img>
         <img src={MuiLogo} alt="MUI logo"></img>
+        <img src={FigmaLogo} alt="Figma logo"></img>
+        <img src={PsLogo} alt="Photoshop logo"></img>
       </div>
     </div>
   );
