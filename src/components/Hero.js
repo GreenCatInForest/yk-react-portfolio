bb6d50026f03d0fac2b09610d63fafb4e01a71a3
GreenCatInForest <event2016.2017@gmail.com>
2024-02-10 15:00:17 +0000

    add JS react logo to Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,10 @@
 import { SocialMedia } from "./SocialMedia";
 
 // import bioImg from "../assets/images/Untitled3.png";
-import bioImg from "../assets/images/Group11.png";
-// import bioImg from "../assets/images/Group3.png";
-
+// import bioImg from "../assets/images/Group11.png";
+import bioImg from "../assets/images/Group3.png";
+import JsLogo from "../assets/images/tech/icons8-javascript-240.svg";
+import ReactLogo from "../assets/images/tech/logo192.png";
 import CVDocument from "../assets/downloads/CV_Front_End_Developer_Engineer.pdf";
 
 export const Hero = () => {
@@ -24,7 +25,16 @@ export const Hero = () => {
         </button>
         <SocialMedia />
       </article>
+
       <img src={bioImg} alt="my large avatar" className="heroImage" />
+      <div className="heroLogoContainer">
+        <img src={JsLogo} alt="JS logo near the avatar" id="heroJSLogo" />
+        <img
+          src={ReactLogo}
+          alt="React logo near the avatar"
+          id="heroReactLogo"
+        />
+      </div>
     </div>
   );
 };
