d6748fe7616cc4be1a0549283285cab6715a76a4
GreenCatInForest <event2016.2017@gmail.com>
2024-02-10 22:49:48 +0000

    add useffect manage the darkmode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { Projects } from "./containers/Projects";
 import { ContactMe } from "./containers/ContactMe";
@@ -11,14 +11,25 @@ import { Footer } from "./components/Footer";
 export const App = () => {
   const [darkMode, setDarkMode] = useState(false);
 
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("darkMode");
+      document.body.classList.remove("lightMode");
+    } else {
+      document.body.classList.add("lightMode");
+      document.body.classList.remove("darkMode");
+    }
+    return () => {};
+  }, [darkMode]);
+
   return (
-    <div className={`appContainer ${darkMode ? "darkMode" : "lightMode"}`}>
+    <div className="appContainer">
       <Header setDarkMode={setDarkMode} darkMode={darkMode} />
-      <Hero />
-      <Projects />
+      <Hero darkMode={darkMode} />
+      <Projects darkMode={darkMode} />
       <TechStack />
       <ContactMe darkMode={darkMode} />
-      <Footer />
+      <Footer darkMode={darkMode} />
     </div>
   );
 };
