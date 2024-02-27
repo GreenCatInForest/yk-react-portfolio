d76dcc2a202dd8c2ecdf25533fe102376f8c4673
GreenCatInForest <event2016.2017@gmail.com>
2024-02-27 22:03:26 +0000

    add rating functionality

--- src/components/RatingForm.js
+++ src/components/RatingForm.js
@@ -1,24 +1,53 @@
+import { useState } from "react";
+import starEmpty from "../assets/images/icons8-star-empty.png";
+import starFilled from "../assets/images/icons8-star-fill.png";
+
 export const RatingForm = () => {
+  const [rating, setRating] = useState(0);
+  const [hoverRating, setHoverRating] = useState(0);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleRatingClick = (value) => {
+    if (!isSubmitted) {
+      setRating(value);
+    }
+  };
+
+  const handleStarHover = (value) => {
+    if (!isSubmitted) {
+      setHoverRating(value);
+    }
+  };
+
+  const handleSubmit = () => {
+    setIsSubmitted(true);
+    alert(`You have rated: ${rating}`);
+  };
+
   return (
     <div className="ratePortfolio">
       <h3 className="componentTitle">Rate my portfolio:</h3>
-      <form action="" method="post" id="starRating">
-        <label htmlFor="star5">✩</label>
-        <input id="star5" name="star" type="radio" value="5" />
-        <label htmlFor="star4">✩</label>
-        <input id="star4" name="star" type="radio" value="4" />
-        <label htmlFor="star3">✩</label>
-        <input id="star3" name="star" type="radio" value="3" />
-        <label htmlFor="star2">✩</label>
-        <input id="star2" name="star" type="radio" value="2" />
-        <label htmlFor="star1">✩</label>
-        <input id="star1" name="star" type="radio" value="1" />
-        <div className="clear"></div>
-        {/* <label htmlFor="feedbackRating"></label> */}
-        <button type="submit" className="btn-orange btnRatingForm">
+
+      {[1, 2, 3, 4, 5].map((value) => (
+        <img
+          src={
+            value <= (hoverRating || rating) ? `${starFilled}` : `${starEmpty}`
+          }
+          alt="star"
+          key={value}
+          className={
+            value <= (hoverRating || rating) ? "starFilled" : "starEmpty"
+          }
+          onClick={() => handleRatingClick(value)}
+          onMouseEnter={() => handleStarHover(value)}
+          onMouseLeave={() => handleStarHover(0)}
+        />
+      ))}
+      {!isSubmitted && (
+        <button onClick={handleSubmit} className="btn-orange btnRatingForm">
           Rate Now
         </button>
-      </form>
+      )}
     </div>
   );
 };
