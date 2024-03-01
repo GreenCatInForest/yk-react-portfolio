1b0f4a5edd3db28bc38550c333dd3159c5b13c0a
GreenCatInForest <event2016.2017@gmail.com>
2024-03-01 18:30:57 +0000

    add nodemailer

---
+++ backend-node/sendEmail.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "proton.me",
+  auth: {
+    user: "yana-portfolio@proton.me",
+    pass: "00000000",
+  },
+});
+
+const sendEmail = async (contactData) => {
+  const message = {
+    from: "yana-portfolio@proton.me",
+    to: "yanaportfoliofullstack@gmail.com",
+    subject: "New Form Submission",
+    text: JSON.stringify(contactData),
+  };
+
+  try {
+    await transporter.sendMail(message);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
+
+module.exports = sendEmail;
