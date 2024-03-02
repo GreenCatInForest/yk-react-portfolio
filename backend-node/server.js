76038e9c761a5695feaca26f59a63a20da89d22d
GreenCatInForest <event2016.2017@gmail.com>
2024-03-02 16:44:39 +0000

    add check

--- backend-node/server.js
+++ backend-node/server.js
@@ -65,6 +65,9 @@ app.post("/submit", async (req, res) => {
   }
 });
 
+// app.listen(5000, () => {
+//   console.log("Server is running on http://localhost:5000");
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
